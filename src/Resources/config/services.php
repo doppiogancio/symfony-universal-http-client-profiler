04ff4eed987eb714c475ce21eebd91ac01632879
Fabrizio Gargiulo <fabrizio.gargiulo@gmail.com>
2025-12-07 17:22:11 +0100

    Add CLI sessions listing to profiler

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -3,6 +3,7 @@
 use HttpProfiler\Session\ContextDetector;
 use HttpProfiler\Session\ConsoleSubscriber;
 use HttpProfiler\Session\SessionManager;
+use HttpProfiler\Session\SessionReader;
 use HttpProfiler\Storage\TraceStorage;
 use HttpProfiler\Tracer\HttpClientTracer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -27,6 +28,8 @@ return static function (ContainerConfigurator $container): void {
         ]);
 
     $services->set(SessionManager::class);
+
+    $services->set(SessionReader::class);
 
     $services->set(ConsoleSubscriber::class)
         ->tag('kernel.event_subscriber');
