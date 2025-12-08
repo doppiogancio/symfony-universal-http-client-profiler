d2b80efa3fa2c140ea6cc161c3a0e259735a8911
Fabrizio Gargiulo <fabrizio.gargiulo@gmail.com>
2025-12-08 01:50:36 +0100

    Add PHPUnit setup and CI workflow

---
+++ tests/Storage/TraceStorageTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Universal\HttpClientProfiler\Tests\Storage;
+
+use PHPUnit\Framework\TestCase;
+
+class TraceStorageTest extends TestCase
+{
+    public function testDummy(): void
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Tracer/HttpClientTracerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Universal\HttpClientProfiler\Tests\Tracer;
+
+use PHPUnit\Framework\TestCase;
+
+class HttpClientTracerTest extends TestCase
+{
+    public function testDummy(): void
+    {
+        $this->assertTrue(true);
+    }
+}
